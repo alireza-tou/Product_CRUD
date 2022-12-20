dfef620f53fd65261a63ada269507981a42a5f6e
alireza tourany <82014996+alireza-tou@users.noreply.github.com>
2022-12-20 22:58:51 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+from product import Product
+item1=Product(1,"iphone 12 mini","phone","apple mobile phone","iphone-12","https://www.apple.com/shop/buy-iphone/iphone-12",True,"A2176",600,600,799,20,20,True,"2022/12/15","2022/12/20")
+print(item1) # __str__
--- product.py
+++ product.py
@@ -7,13 +7,14 @@ pk= Primary Key
 """
 
 class Product():
-    def __init__(self,category_id:int,title:str,short_description:str,description:str,slug,permalink:str,is_available:bool,
+    identity=[]
+    def __init__(self,category_id:int,title:str,short_description:str,description:str,slug:str,permalink:str,is_available:bool,
 
                 sku:str,price:float,regular_price:float,sale_price:float,manage_stock:int,stock_quantity:int,is_visibla:bool,
         
                 date_created_gmt:str,date_modified_gmt:str):
         
-        
+        Product.identity.append(self)
         self.category_id =category_id
         self.title = title
         self.short_description = short_description
@@ -31,16 +32,19 @@ class Product():
         self.date_created_gmt = date_created_gmt
         self.date_modified_gmt = date_modified_gmt
 
-    def update(self):
-        pass
+    def read(self):
+        print(self.__repr__())
 
     def delete(self):
-        pass
+        #del self
+        Product.identity.remove(self)
 
-    def read(self):
-        pass
+    def update(self,**kwrgs):
+        for i,j in kwrgs:
+            self.i=j
 
-    def __repr__(self):
+
+    def __repr__(self)-> str:
         return f"Product [ {self.category_id} {self.title} {self.short_description} {self.description} {self.slug} {self.permalik} {self.permalik} {self.is_available} {self.sku} {self.price} {self.regular_price} {self.sale_price} {self.manage_stock} {self.stock_quantity} {self.is_visibla} {self.date_created_gmt} {self.date_modified_gmt} ]"
 
     """
@@ -56,3 +60,5 @@ class Product():
         \"""%(self.title,self.category_id,self.description,self.is_available,
         self.regular_price,self.sale_price,self.date_created_gmt,self.date_modified_gmt))"""
         
+    def __str__(self) -> str:
+        return f"{self.category_id} {self.title} {self.short_description} {self.description} {self.slug} {self.permalik} {self.permalik} {self.is_available} {self.sku} {self.price} {self.regular_price} {self.sale_price} {self.manage_stock} {self.stock_quantity} {self.is_visibla} {self.date_created_gmt} {self.date_modified_gmt} "
