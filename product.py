fd5fb7154cc5c8c5499e9681233275943f6844f5
alireza tourany <82014996+alireza-tou@users.noreply.github.com>
2022-12-20 21:56:24 +0330

    Update product.py

--- product.py
+++ product.py
@@ -7,12 +7,13 @@ pk= Primary Key
 """
 
 class Product():
-    def __init__(self,category_id:int,title:str,short_description:str,description:str,slug:str,permalink:str,is_available:bool,
+    def __init__(self,category_id:int,title:str,short_description:str,description:str,slug,permalink:str,is_available:bool,
 
                 sku:str,price:float,regular_price:float,sale_price:float,manage_stock:int,stock_quantity:int,is_visibla:bool,
         
                 date_created_gmt:str,date_modified_gmt:str):
-
+        
+        
         self.category_id =category_id
         self.title = title
         self.short_description = short_description
@@ -39,8 +40,12 @@ class Product():
     def read(self):
         pass
 
+    def __repr__(self):
+        return f"Product [ {self.category_id} {self.title} {self.short_description} {self.description} {self.slug} {self.permalik} {self.permalik} {self.is_available} {self.sku} {self.price} {self.regular_price} {self.sale_price} {self.manage_stock} {self.stock_quantity} {self.is_visibla} {self.date_created_gmt} {self.date_modified_gmt} ]"
+
+    """
     def __repr__(self) :
-        return("""name : %s
+        return(\"""name : %s
         id : %d
         description : %s
         availability : %b
@@ -48,6 +53,6 @@ class Product():
         sale price : %f
         date created : %s
         date modified : %s
-        """%(self.title,self.category_id,self.description,self.is_available,
-        self.regular_price,self.sale_price,self.date_created_gmt,self.date_modified_gmt))
+        \"""%(self.title,self.category_id,self.description,self.is_available,
+        self.regular_price,self.sale_price,self.date_created_gmt,self.date_modified_gmt))"""
         
