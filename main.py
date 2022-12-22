832fa3389f88265b667fad94eed40a49ac38209d
alireza tourany <82014996+alireza-tou@users.noreply.github.com>
2022-12-22 19:31:28 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from product import Product
 item1=Product(1,"iphone 12 mini","phone","apple mobile phone","iphone-12","https://www.apple.com/shop/buy-iphone/iphone-12",True,"A2176",600,600,799,20,20,True,"2022/12/15","2022/12/20")
+
+
+
 print(item1) # __str__
 print(Product.all_objects)#__repr__
-print(type(item1)) #<class 'product.Product'>
 
